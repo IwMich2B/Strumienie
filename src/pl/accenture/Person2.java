83e6fd280c0e8769fed4454168215db8efe4f563
IwMich2B <iwmichalak@interia.pl>
2019-12-10 10:37:42 +0100

    dodanie klasy

--- src/pl/accenture/Person2.java
+++ src/pl/accenture/Person2.java
@@ -1,10 +1,10 @@
 package pl.accenture;
-// zmiana w klasie person
+
 
 public class Person2 {
     String name;
     String lastName;
-    // konstruktor z dwoma parametrami
+
 
     public Person2(String name, String lastName) {
         this.name = name;
