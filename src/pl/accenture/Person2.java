f399fe0f15deaf5d5ca07e3b97df6515d3f779b8
IwMich2B <iwmichalak@interia.pl>
2019-12-10 10:06:01 +0100

    Cwiczenia z strumieniami Rozbudowanie projektu Dodana klasa Person2

---
+++ src/pl/accenture/Person2.java
@@ -0,0 +1,19 @@
+package pl.accenture;
+
+public class Person2 {
+    String name;
+    String lastName;
+
+    public Person2(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person2{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
